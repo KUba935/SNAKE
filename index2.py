032d1374b4c77e8afdc9a11837439a68636cd394
KUba935 <kubsonix198@gmail.com>
2025-10-21 12:04:55 +0200

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,22 @@
+while(True):
+    print("> Zwierzęta")
+    print("> Wybierz opcje")
+    print("1) mysz")
+    print("2) wąż")
+    print("3) pies")
+    print("4) kot")
+    print("q) Potwierdzam")
+    Zwierzę = input("wybierz zwierze: ")
+    if Zwierzę == 'q':
+       break
+
+
+while(True):
+    print("Snake")
+    print("<start>")
+    print("<opcje>")
+    print("wyjdz")
+    Snake = input("wybierz opcje: ")
+    if Snake == 'w' :
+         break
+    
